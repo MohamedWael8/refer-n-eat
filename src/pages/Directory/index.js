411133a1dbef583bbd30557eb0e231620cc419b5
Mohamed Wa'el <45937466+MohamedWael8@users.noreply.github.com>
2022-01-06 21:00:14 +0200

    Base Structure parser adjustment tailwind api prep

---
+++ src/pages/Directory/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useAxios } from 'use-axios-client';
+
+import RestaurantCard from './RestaurantCard';
+import axiosInstance from '../../api/apiInstance';
+import { RESTAURANTS } from '../../api/RestaurantAPI';
+
+const restaurants = ['Some', 'Things', 'Never', 'Change'];
+
+const Directory = () => {
+  const { data, error, loading } = useAxios({
+    axiosInstance: axiosInstance,
+    url: RESTAURANTS,
+  });
+
+  console.log(data, error, loading);
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {restaurants.map(item => (
+        <div key={`${item}`}>
+          <RestaurantCard />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
